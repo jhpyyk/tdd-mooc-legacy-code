2d86445133e7ba422aabdf30963437e1c8daf993
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-13 12:11:37 +0300

    Refactor extract nested if

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -29,15 +29,16 @@ export class Shop {
 
       if (this.items[i].quality < 50 && this.items[i].name === "Backstage passes to a TAFKAL80ETC concert") {
         this.items[i].quality = this.items[i].quality + 1;
-        if (
-          this.items[i].name === "Backstage passes to a TAFKAL80ETC concert" &&
-          this.items[i].sellIn < 11 &&
-          this.items[i].quality < 50
-        ) {
+      }
+
+      if (
+        this.items[i].name === "Backstage passes to a TAFKAL80ETC concert" &&
+        this.items[i].sellIn < 11 &&
+        this.items[i].quality < 50
+      ) {
+        this.items[i].quality = this.items[i].quality + 1;
+        if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
-          if (this.items[i].sellIn < 6 && this.items[i].quality < 50) {
-            this.items[i].quality = this.items[i].quality + 1;
-          }
         }
       }
 
