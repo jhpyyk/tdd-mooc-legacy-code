ae35b3aec4a3dddc81985590a9c3fb3c70c8da42
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-13 11:15:39 +0300

    Refactor merge if clauses

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -23,10 +23,8 @@ export class Shop {
         if (this.items[i].quality < 50) {
           this.items[i].quality = this.items[i].quality + 1;
           if (this.items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
-              }
+            if (this.items[i].sellIn < 11 && this.items[i].quality < 50) {
+              this.items[i].quality = this.items[i].quality + 1;
             }
             if (this.items[i].sellIn < 6) {
               if (this.items[i].quality < 50) {
