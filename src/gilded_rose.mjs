b36c4bd25e4a41f59b6b04971b3846a1f0e0a34b
Juuso Pyykkönen <juuso.pyyk@gmail.com>
2025-09-13 11:23:48 +0300

    Refactor extract and flip if from nested

--- src/gilded_rose.mjs
+++ src/gilded_rose.mjs
@@ -35,18 +35,19 @@ export class Shop {
       if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != "Aged Brie") {
-          if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-            if (this.items[i].quality > 0 && this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-              this.items[i].quality = this.items[i].quality - 1;
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+      if (this.items[i].sellIn >= 0) {
+        return this.items;
+      }
+      if (this.items[i].name != "Aged Brie") {
+        if (this.items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
+          if (this.items[i].quality > 0 && this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+            this.items[i].quality = this.items[i].quality - 1;
           }
-        } else if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1;
+        } else {
+          this.items[i].quality = this.items[i].quality - this.items[i].quality;
         }
+      } else if (this.items[i].quality < 50) {
+        this.items[i].quality = this.items[i].quality + 1;
       }
     }
 
